70d827e653aec8c575e5b103edd418af10112316
root <ikseon1026@gmail.com>
2015-03-29 22:29:11 +0900

    수정 내용 적용

--- part1/5/3.c
+++ part1/5/3.c
@@ -2,16 +2,16 @@
 
 int main(void)
 {
-	char a[] = {'h','e','l','l','o'};	
+	char a[] = {'a','b','c','d'};	
 	char *p = a;	
 
-	printf("a[n] : %c %c %c %c %c\n", a[0],a[1],a[2],a[3],a[4]);
-	printf("a+n : %p, %p, %p, %p, %p\n", a+0,a+1,a+2,a+3,a+4);
-	printf("*(a+n) : %c %c %c %c %c\n", *(a+0), *(a+1), *(a+2),*(a+3),*(a+4));
+	printf("a[n] : %c %c %c %c\n", a[0],a[1],a[2],a[3]);
+	printf("a+n : %p, %p, %p, %p\n", a+0,a+1,a+2,a+3);
+	printf("*(a+n) : %c %c %c %c\n", *(a+0), *(a+1), *(a+2),*(a+3));
 
-	printf("p[n] :%c %c %c %c %c\n", p[0],p[1],p[2],p[3],p[4]);
-	printf("p+n : %p, %p, %p, %p, %p\n", p+0,p+1,p+2,p+3,p+4);
-	printf("*(p+n) : %c %c %c %c %c\n", *(p+0), *(p+1), *(p+2),*(p+3),*(p+4));
+	printf("p[n] :%c %c %c %c\n", p[0],p[1],p[2],p[3]);
+	printf("p+n : %p, %p, %p, %p\n", p+0,p+1,p+2,p+3);
+	printf("*(p+n) : %c %c %c %c\n", *(p+0), *(p+1), *(p+2),*(p+3));
 
 	return 0;
 }				
