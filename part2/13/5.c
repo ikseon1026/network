543ecd4466ec6425cf102d9b45b1e21ade3b212b
root <ikseon1026@gmail.com>
2015-03-29 23:20:22 +0900

    출력되는 내용 수정

--- part2/13/5.c
+++ part2/13/5.c
@@ -7,7 +7,7 @@
 	#include <arpa/inet.h>
 #endif
 
-void print2hex(const char *msg, void *p, size_t len);
+void print2hex(void *p, size_t len);
 
 int main(void)
 {
@@ -16,17 +16,15 @@ int main(void)
 	addr.s_addr = inet_addr("127.0.0.1");
 
 	printf("inet_addr : %08x\n", addr.s_addr);
-	print2hex("inet_addr", &addr.s_addr, sizeof(addr.s_addr));
+	print2hex(&addr.s_addr, sizeof(addr.s_addr));
 	printf("inet_ntoa : %s\n", inet_ntoa(addr));
 
 	return 0;
 }				
 
-void print2hex(const char *msg, void *p, size_t len)
+void print2hex(void *p, size_t len)
 {
 	size_t i;
-
-	printf("%s : ", msg);
 		
 	for(i = 0; i < len; i += 1) {
 		printf("[%p]%x ", ((char*)p+i), *((char *)p+i));	
